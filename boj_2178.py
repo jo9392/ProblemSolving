6aee0843e3726021c3927dec0724ca05519f4fca
sujeong <jo9392@naver.com>
2023-03-16 16:12:33 +0900

    edit the solution

--- boj_2178.py
+++ boj_2178.py
@@ -1,71 +1,30 @@
 global maze_map
-global temp_pos
+from collections import deque
 
 def save_map():
     global maze_map
-    input_val = list(map(int, input().split()))
-    x, y = map(int, input_val[0:2])
-    input_map = input_val[2:]
-    print(input_map)
+    x, y = map(int, input().split())
     maze_map = []
     for i in range(x):
-        maze_map.append([int(j) for j in str(input_map[i])])
-    # list_map = list(map(int, input()))
-    # maze_map = []
-    # for i in range(0, len(list_map), y):
-    #     maze_map.append(list_map[i:i + y])
+        maze_map.append([int(j) for j in input()])
     return (x-1, y-1)
 
-def move_position(move):
-    global maze_map
-    global temp_pos
-    maze_map[temp_pos[0]][temp_pos[1]] = 0
-    if move == 'down':
-        temp_pos = (temp_pos[0]+1, temp_pos[1])
-    elif move == 'right':
-        temp_pos = (temp_pos[0], temp_pos[1]+1)
-    elif move == 'up':
-        temp_pos = (temp_pos[0]-1, temp_pos[1])
-    elif move == 'left':
-        temp_pos = (temp_pos[0], temp_pos[1]-1)
-    else:
-        print('wrong element!')
 
-def index_error_flag(pos):
-    try:
-        maze_map[pos[0]][pos[1]]
-        return 0
-    except:
-        return 1
-    return false
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     global maze_map
-    global temp_pos
     des_pos = save_map()
-    temp_pos = (0,0)
     count = 0
-    print(maze_map)
-    while(temp_pos != des_pos):
-        if (not index_error_flag((temp_pos[0]+1,temp_pos[1]))) and maze_map[temp_pos[0]+1][temp_pos[1]] == 1:
-            move_position('down')
-            print("down\n")
-            count+=1
-        elif (not index_error_flag((temp_pos[0],temp_pos[1]+1))) and maze_map[temp_pos[0]][temp_pos[1]+1] == 1:
-            move_position('right')
-            print("right\n")
-            count += 1
-        elif (not index_error_flag((temp_pos[0]-1,temp_pos[1]))) and maze_map[temp_pos[0]-1][temp_pos[1]] == 1:
-            move_position('up')
-            print("up\n")
-            count += 1
-        elif (not index_error_flag((temp_pos[0],temp_pos[1]-1))) and maze_map[temp_pos[0]][temp_pos[1]-1] == 1:
-            move_position('left')
-            print("left\n")
-            count += 1
-        else :
-            print("something gone wrong!\n")
+    dq = deque()
+    dq.append([(0,0), count])
+    pos_change = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    while(dq):
+        temp_queue = dq.popleft()
+        temp_pos = temp_queue[0]
+        if temp_pos == des_pos:
+            print(temp_queue[1]+1)
             break
-    print((temp_pos[0]+1, temp_pos[1]+1), count+1)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        for i in pos_change:
+            next_pos = tuple(sum(elem) for elem in zip(temp_pos, i))
+            if 0<= next_pos[0] <= des_pos[0] and 0 <= next_pos[1] <= des_pos[1] and maze_map[next_pos[0]][next_pos[1]] == 1:
+                maze_map[next_pos[0]][next_pos[1]] = 0
+                dq.append([next_pos, temp_queue[1]+1])
