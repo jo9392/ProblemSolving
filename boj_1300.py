f5019e1aad8ec9e0aa22e27d289a33d33f67a859
jo9392 <jo9392@naver.com>
2023-03-18 20:27:39 +0900

    solve 1300

---
+++ boj_1300.py
@@ -0,0 +1,24 @@
+def count_small_num(N, s_num):
+    s_num -= 1
+    count = 0
+    for i in range(1, N + 1):
+        count += min(s_num // i, N)
+    return count+1
+
+
+if __name__ == '__main__':
+    N = int(input())
+    k = int(input())
+    count = 0
+    s_num = int(k/2)
+    left = 1
+    right = k
+    while(left <= right):
+        count = count_small_num(N, s_num)
+        if count > k:
+            right = s_num - 1
+        else:
+            left = s_num + 1
+        s_num = int((left+right)/2)
+
+    print(s_num)
