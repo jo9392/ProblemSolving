b25cb24fa9f27ef13131472ee33619aa9fe07703
jo9392 <jo9392@naver.com>
2023-03-22 20:45:11 +0900

    edit input line

--- greedy_search/boj_1931.py
+++ greedy_search/boj_1931.py
@@ -1,16 +1,18 @@
-if __name__ == "__main__":
-    node_cnt = int(input())
-    meet_list = []
-    for i in range(node_cnt):
-        (x, y) = map(int, input().split())
-        meet_list.append((x, y))
+import sys
 
-    meet_list.sort(key = lambda meet_list:(meet_list[1], meet_list[0]))
-    cnt = end_time = 0
+input = sys.stdin.readline
+meet_list = []
 
-    for i in range(len(meet_list)):
-        if meet_list[i][0] >= end_time:
-            cnt += 1
-            end_time = meet_list[i][1]
+for i in range(int(input())):
+    (x, y) = map(int, input().split())
+    meet_list.append((x, y))
 
-    print(cnt)
+meet_list.sort(key = lambda meet_list:(meet_list[1], meet_list[0]))
+cnt = end_time = 0
+
+for i in range(len(meet_list)):
+    if meet_list[i][0] >= end_time:
+        cnt += 1
+        end_time = meet_list[i][1]
+
+print(cnt)
