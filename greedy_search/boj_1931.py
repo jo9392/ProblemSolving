d93228e338d38620aae3d3c5b34e263d8d545778
jo9392 <jo9392@naver.com>
2023-03-22 20:35:34 +0900

    solution update

--- greedy_search/boj_1931.py
+++ greedy_search/boj_1931.py
@@ -1,26 +1,16 @@
-from collections import deque
-def has_duplicates(seq):
-    return len(seq) != len(set(seq))
-
 if __name__ == "__main__":
     node_cnt = int(input())
-    all_meet_list = []
+    meet_list = []
     for i in range(node_cnt):
         (x, y) = map(int, input().split())
-        all_meet_list.append((x, y))
-    sorted_list = sorted(all_meet_list, key=lambda all_meet_list:all_meet_list[1])
-    sorted_dq = deque(sorted_list)
-    cnt = 0
-    end_time = 0
-    while(sorted_dq):
-        temp_dq = sorted_dq.popleft()
-        end_time = temp_dq[1]
-        # temp_dq와 겹치는 스케줄을 전부 삭제하는 코드
-        for i in range(len(sorted_dq)):
-            if sorted_dq[0][0] < end_time:
-                sorted_dq.
-            else:
-                break
-        cnt += 1
+        meet_list.append((x, y))
+
+    meet_list.sort(key = lambda meet_list:(meet_list[1], meet_list[0]))
+    cnt = end_time = 0
+
+    for i in range(len(meet_list)):
+        if meet_list[i][0] >= end_time:
+            cnt += 1
+            end_time = meet_list[i][1]
 
     print(cnt)
