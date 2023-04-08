12deaaa4fd705d780628eadbef17faeb247eda00
jo9392 <jo9392@naver.com>
2023-04-08 17:17:25 +0900

    edit solution

--- greedy_search/boj_1783.py
+++ greedy_search/boj_1783.py
@@ -1,25 +1,13 @@
 import sys
-from collections import deque
 
 if __name__=="__main__":
     input = sys.stdin.readline
-    length, width = map(int, input().split())
+    width, length = map(int, input().split())
 
-    pos_change=[(1,2), (2,1), (1,-2), (2,-1)]
-    pos = (0,0)
-    cnt=1
-    dq = deque()
-    dq.append([pos, cnt])
-
-    # for i in range(2):
-    while(dq):
-        temp_dq = dq.popleft()
-        for pc in pos_change:
-            temp_pos = temp_dq[0]
-            temp_cnt = temp_dq[1]
-            next_pos = tuple(sum(elem) for elem in zip(temp_pos, pc))
-            if 0<=next_pos[0]<width and 0<=next_pos[1]<length:
-                temp_cnt += 1
-                dq.append([next_pos, temp_cnt])
-
-    print(temp_cnt)
+    if length < 6 or width < 2:
+        if width == 1:
+            print(min(int(length/2)+1, 4))
+        else:
+            print(min(length+1,4))
+    else:
+        print(length-1)
