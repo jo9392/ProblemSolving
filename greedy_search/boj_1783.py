3294b1eb4f14afdd09949b8d126ef133f618d338
sujeong <jo9392@naver.com>
2023-04-08 18:57:12 +0900

    1783 solution

--- greedy_search/boj_1783.py
+++ greedy_search/boj_1783.py
@@ -3,10 +3,14 @@ import sys
 if __name__=="__main__":
     input = sys.stdin.readline
     width, length = map(int, input().split())
+    width -= 1
+    length -= 1
 
     if length < 6 or width < 2:
         if width == 1:
             print(min(int(length/2)+1, 4))
+        elif width == 0:
+            print(1)
         else:
             print(min(length+1,4))
     else:
