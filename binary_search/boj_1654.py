f2338e1d89da7fcfec1632b1169f7766ed21815c
sujeong <jo9392@naver.com>
2023-04-26 17:36:16 +0900

    1654 solution
    이분탐색

--- binary_search/boj_1654.py
+++ binary_search/boj_1654.py
@@ -1,7 +1,30 @@
 import sys
 input = sys.stdin.readline
 k, n = map(int, input().split())
-init_line_list = []
-for _ in range(k): init_line_list.append(int(input()))
+input_line_list = []
+for _ in range(k): input_line_list.append(int(input()))
 
-optimum = int(sum(init_line_list) / n)
+optimum = int(sum(input_line_list) / n)
+
+left = 1
+right = optimum
+
+def can_be_mid(n, input_line_list, mid):
+    line_cnt = 0
+    for i in input_line_list:
+        line_cnt += int(i/mid)
+    if line_cnt >= n:
+        return True
+    else:
+        return False
+
+while(True):
+    mid = int((left+right)/2)
+    if can_be_mid(n, input_line_list, mid):
+        left = mid+1
+    else:
+        right = mid-1
+    if left > right : break
+
+mid = int((left+right)/2)
+print(mid)
