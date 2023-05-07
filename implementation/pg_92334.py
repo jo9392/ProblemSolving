7b15585a5c2ca91ccee6f8f3b60706c0fdb25d83
jo9392 <jo9392@naver.com>
2023-05-07 20:44:59 +0900

    solution 수정

--- implementation/pg_92334.py
+++ implementation/pg_92334.py
@@ -2,13 +2,13 @@ from collections import defaultdict
 
 
 def solution(id_list, report, k):
-    reporter_dict = defaultdict(set)  # 유저별 유저가 신고한 사람
+    reporter_dict = defaultdict(list)  # 유저별 유저가 신고한 사람
     reported_dict = defaultdict(int)  # 유저별 신고당한 횟수
     report_set = list(set(report))
 
     for re in report_set:
         reporter, reported = re.split()
-        reporter_dict[reporter].add(reported)
+        reporter_dict[reporter].append(reported)
         reported_dict[reported] += 1
 
     answer = [0] * len(id_list)
