d79ca05fc4cb9b7d67b95f340c81bd9e759c9cd5
sujeong <jo9392@naver.com>
2023-05-15 03:12:31 +0900

    solution 17680

---
+++ implementation/pg_17680.py
@@ -0,0 +1,21 @@
+# collections import deque
+# deque, list는 remove(c)로 특정 원소를 삭제하는 기능이 있다
+
+from collections import deque
+def solution(cacheSize, cities):
+    cache = deque("" for _ in range(cacheSize))
+    answer = 0
+    if cacheSize == 0:
+        answer = len(cities)*5
+    else:
+        for c in cities:
+            c = c.lower()
+            if c in cache:
+                cache.remove(c)
+                cache.append(c)
+                answer += 1
+            else:
+                cache.popleft()
+                cache.append(c)
+                answer += 5
+    return answer
